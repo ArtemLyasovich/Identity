dd34f829d8456dd2695934cfb54420b33206233a
Huawei <artemlysovich@gmail.com>
2023-08-10 10:07:51 +0300

    Commit

---
+++ Areas/Identity/Data/ApplicationUserEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventCalendar.Areas.Identity.Data;
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventCalendar.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            ViewBag.UserName = User.Identity.Name;
+        }
+
+        return View();
+    }
+}
